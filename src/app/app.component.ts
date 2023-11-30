3d7dffad2c3212cf845462b697e371a1ea122c53
pandurangrane16 <panduerangrane16@gmail.com>
2023-11-30 23:37:56 +0530

    30-11-2023 Part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { UserFacadeService } from './facade/facade_services/user-facade.service'
 import { User } from './models/response/User';
 import { Router } from '@angular/router';
 import { LocationStrategy } from '@angular/common';
+import { SessionService } from './facade/services/common/session.service';
+import { CommonFacadeService } from './facade/facade_services/common-facade.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,6 +18,7 @@ export class AppComponent implements OnInit {
   loggedIn:boolean=false;
   constructor(private _facadeService:UserFacadeService,
               private _router: Router,
+              private _commonFacade:CommonFacadeService
               ){ 
     console.log(this.loggedIn);
     this._facadeService.isLoggedin.subscribe(x => {
@@ -28,6 +31,7 @@ export class AppComponent implements OnInit {
   
   ngOnInit(): void {
     this.user = this._facadeService.user;
+    this._commonFacade.setSwaggerUrl();
     //this._facadeService.getConfigDetails();
   }
 }
